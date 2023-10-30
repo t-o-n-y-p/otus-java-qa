73ece3672fdb0742cea71321caef5259d25c7728
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2023-10-30 14:44:42 +0300

    removed hw 1 and 2 from the branch

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
 Предусловия:
 - иметь Java 17+
-- для д.з. 1 и 2 иметь браузеры Chrome и Firefox версий, бьющихся с версиями драйверов
-
-Варианты запуска ДЗ 1 в зависимости от желаемого браузера:
-- mvn test -P chrome
-- mvn test -P firefox
-
-Запуск ДЗ 2:
-- mvn test -P cucumber
 
 Запуск ДЗ 3:
 - mvn test -P petstore
--- src/main/java/org/example/utils/PetStorePath.java
+++ src/main/java/org/example/utils/PetStorePath.java
@@ -8,7 +8,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class PetStorePath {
 
-  public static final String BASE_PATH = "https://petstore.swagger.io/v2";
+  public static final String BASE_PATH = System.getProperty("base.url");
   public static final String POST_USER = "/user";
   public static final String USER_BY_USERNAME = "/user/{username}";
   public static final String POST_PET = "/pet";
