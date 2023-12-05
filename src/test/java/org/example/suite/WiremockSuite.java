e8be48cf2004c6507316ab28c67459671a0e7ae8
tonyp <42129780+t-o-n-y-p@users.noreply.github.com>
2023-12-05 22:24:58 +0300

    added container operations

--- src/test/java/org/example/suite/WiremockSuite.java
+++ src/test/java/org/example/suite/WiremockSuite.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.example.containers.WireMockContainer;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
@@ -12,13 +13,18 @@ public class WiremockSuite {
   @Getter
   private static final WireMockContainer wiremock =
       new WireMockContainer(WireMockContainer.WIREMOCK_2_LATEST)
-          .withMappingFromResource("users.json")
-          .withMappingFromResource("courses.json")
-          .withMappingFromResource("score.json");
+          .withMappingFromResource("users", "users.json")
+          .withMappingFromResource("courses", "courses.json")
+          .withMappingFromResource("score", "score.json");
 
   @BeforeAll
   static void beforeAll() {
     wiremock.start();
+  }
+
+  @Test
+  void name() {
+    System.out.println(wiremock.getUrl("/user/get/all"));
   }
 
   @AfterAll
