78d9d7b6b42f85e53cf3cdc56a43f81da595f05e
tonyp <42129780+t-o-n-y-p@users.noreply.github.com>
2023-12-06 23:49:53 +0300

    removed locks on cucumber hooks

--- src/test/java/org/example/hooks/Hooks.java
+++ src/test/java/org/example/hooks/Hooks.java
@@ -4,9 +4,6 @@ import com.google.inject.Inject;
 import io.cucumber.java.After;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.BeforeAll;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 import org.example.factory.WiremockModule;
 import org.example.support.GuiceScoped;
 import org.wiremock.integrations.testcontainers.WireMockContainer;
@@ -16,22 +13,12 @@ import org.wiremock.integrations.testcontainers.WireMockContainer;
  */
 public class Hooks {
 
-  private static final Lock LOCK = new ReentrantLock();
-  private static final AtomicBoolean STARTED = new AtomicBoolean(false);
   @Inject
   private GuiceScoped scoped;
 
   @BeforeAll
   public static void before_all() {
-    LOCK.lock();
-    try {
-      if (!STARTED.get()) {
-        WiremockModule.getWiremockContainer().start();
-        STARTED.set(true);
-      }
-    } finally {
-      LOCK.unlock();
-    }
+    WiremockModule.getWiremockContainer().start();
   }
 
   @AfterAll
