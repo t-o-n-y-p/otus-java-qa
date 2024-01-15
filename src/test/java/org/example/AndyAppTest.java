f4a6ee966b9d13da5340353bb19abec071e50101
tonyp <42129780+t-o-n-y-p@users.noreply.github.com>
2024-01-15 20:07:22 +0300

    checkstyle

--- src/test/java/org/example/AndyAppTest.java
+++ src/test/java/org/example/AndyAppTest.java
@@ -2,7 +2,6 @@ package org.example;
 
 import com.google.inject.Inject;
 import java.util.List;
-
 import io.qameta.allure.Epic;
 import org.example.components.ChatMessage;
 import org.example.components.MainMenu;
