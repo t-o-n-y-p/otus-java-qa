957712f0e3cc7610e0394b31e68ce1a7c026d182
tonyp <42129780+t-o-n-y-p@users.noreply.github.com>
2024-01-15 19:31:19 +0300

    updated pipeline and allure report

--- src/test/java/org/example/JsonMockTest.java
+++ src/test/java/org/example/JsonMockTest.java
@@ -3,15 +3,20 @@ package org.example;
 import static org.hamcrest.Matchers.greaterThan;
 
 import com.google.inject.Inject;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
 import org.example.clients.JsonCourseMockClient;
 import org.example.clients.JsonUserMockClient;
 import org.example.extensions.WiremockExtension;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @ExtendWith(WiremockExtension.class)
+@Epic("Тесты API заглушек Wiremock")
+@Story("JSON")
 public class JsonMockTest {
 
   @Inject
@@ -21,6 +26,7 @@ public class JsonMockTest {
   private JsonCourseMockClient jsonCourseMockClient;
 
   @Test
+  @DisplayName("Проверка заглушки для списка пользователей")
   void testAllUsers() {
     jsonUserMockClient
         .getAllUsers()
@@ -29,11 +35,13 @@ public class JsonMockTest {
 
   @ParameterizedTest
   @ValueSource(ints = {1, 23, 456})
+  @DisplayName("Проверка заглушки для оценки пользователя по ID")
   void testUserScore(int id) {
     jsonUserMockClient.getUserScore(id);
   }
 
   @Test
+  @DisplayName("Проверка заглушки для списка курсов")
   void testAllCourses() {
     jsonCourseMockClient
         .getAllCourses()
--- src/test/java/org/example/XmlMockTest.java
+++ src/test/java/org/example/XmlMockTest.java
@@ -3,15 +3,20 @@ package org.example;
 import static org.hamcrest.Matchers.greaterThan;
 
 import com.google.inject.Inject;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
 import org.example.clients.XmlCourseMockClient;
 import org.example.clients.XmlUserMockClient;
 import org.example.extensions.WiremockExtension;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @ExtendWith(WiremockExtension.class)
+@Epic("Тесты API заглушек Wiremock")
+@Story("XML")
 public class XmlMockTest {
 
   @Inject
@@ -21,6 +26,7 @@ public class XmlMockTest {
   private XmlCourseMockClient xmlCourseMockClient;
 
   @Test
+  @DisplayName("Проверка заглушки для списка пользователей")
   void testAllUsers() {
     xmlUserMockClient
         .getAllUsers()
@@ -29,11 +35,13 @@ public class XmlMockTest {
 
   @ParameterizedTest
   @ValueSource(ints = {1, 23, 456})
+  @DisplayName("Проверка заглушки для оценки пользователя по ID")
   void testUserScore(int id) {
     xmlUserMockClient.getUserScore(id);
   }
 
   @Test
+  @DisplayName("Проверка заглушки для списка курсов")
   void testAllCourses() {
     xmlCourseMockClient
         .getAllCourses()
