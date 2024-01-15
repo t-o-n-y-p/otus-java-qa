876f4803558efa023f4d5fe688f9b97b6a35df63
tonyp <42129780+t-o-n-y-p@users.noreply.github.com>
2024-01-15 19:05:21 +0300

    added test names

--- src/test/java/org/example/FindProgramTest.java
+++ src/test/java/org/example/FindProgramTest.java
@@ -1,10 +1,12 @@
 package org.example;
 
 import com.google.inject.Inject;
+import io.qameta.allure.Story;
 import org.apache.commons.lang3.StringUtils;
 import org.example.extensions.GuiceExtension;
 import org.example.pages.LessonPage;
 import org.example.pages.MainPage;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,6 +16,7 @@ import org.junit.jupiter.params.provider.ValueSource;
  .
  */
 @ExtendWith(GuiceExtension.class)
+@Story("UI тесты сайта Otus на Selenoid")
 public class FindProgramTest {
 
   @Inject
@@ -27,6 +30,7 @@ public class FindProgramTest {
       "Специализация Java-разработчик",
       "Специализация Administrator Linux"
   })
+  @DisplayName("Выбор специализации по названию")
   void testProgramByName(String programName) {
     mainPage
         .open()
@@ -37,6 +41,7 @@ public class FindProgramTest {
   }
 
   @Test
+  @DisplayName("Выбор специализации, стартующей раньше всех")
   void testProgramByEarliestStartDate() {
     mainPage
         .open()
@@ -45,6 +50,7 @@ public class FindProgramTest {
   }
 
   @Test
+  @DisplayName("Выбор специализации, стартующей позже всех")
   void testProgramByLatestStartDate() {
     mainPage
         .open()
