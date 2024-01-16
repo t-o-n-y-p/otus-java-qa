3151745ff57566bdb91d51cdf52a06d61430de04
tonyp <42129780+t-o-n-y-p@users.noreply.github.com>
2024-01-16 11:18:22 +0300

    fixed pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -31,6 +31,7 @@ node('maven') {
         stage("Send to Telegram") {
             def summary = junit testResults: "**/target/surefire-reports/*.xml"
             String message = """Test Summary
+                               |JOB: ${env.JOB_NAME}
                                |${currentBuild.description}
                                |
                                |Total: ${summary.totalCount}
